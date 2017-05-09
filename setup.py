6677dd3a6b3ebb7e6ef76d08311d5b8580cdec26
James Lovejoy <jameslovejoy1@gmail.com>
2017-05-09 18:35:24 -0400

    Install old Lyra2RE package

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from distutils.core import setup, Extension
 
 lyra2re2_hash_module = Extension('lyra2re2_hash',
                                sources = [
-										  'lyra2remodule.c',
 										  'lyra2re2module.c',
                                           'Lyra2RE.c',
 										  'Sponge.c',
@@ -15,8 +14,23 @@ lyra2re2_hash_module = Extension('lyra2re2_hash',
 										  'sha3/skein.c'],
                                include_dirs=['.', './sha3'])
 
+lyra2re_hash_module = Extension('lyra2re_hash',
+                               sources = [
+										  'lyra2remodule.c',
+                                          'Lyra2RE.c',
+										  'Sponge.c',
+										  'Lyra2.c',
+										  'sha3/blake.c',
+										  'sha3/groestl.c',
+										  'sha3/keccak.c',
+										  'sha3/cubehash.c',
+										  'sha3/bmw.c',
+										  'sha3/skein.c'],
+                               include_dirs=['.', './sha3'])
+
+
 setup (name = 'lyra2re2_hash',
-       version = '1.0',
+       version = '1.1',
        description = 'Bindings for Lyra2RE2 proof of work used by Vertcoin',
-       ext_modules = [lyra2re2_hash_module])
+       ext_modules = [lyra2re2_hash_module, lyra2re_hash_module])
 
